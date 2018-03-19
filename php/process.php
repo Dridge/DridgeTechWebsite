e7fb7a48a3d309c22916fd61f63df37220c28320
RichardE <richarde@localhost.neversettle.com>
2018-03-19 22:42:39 +0000

    Slight adjustments to php email form

--- php/process.php
+++ php/process.php
@@ -8,17 +8,19 @@ $name = strip_tags($_POST['name']);
 $email = strip_tags($_POST['email']);
 $message = strip_tags($_POST['message']);
 echo "<span class=\"alert alert-success\" >Your message has been received. Thanks! Here is what you submitted:</span><br><br>";
-echo "<stong>Name:</strong> ".$name."<br>";   
-echo "<stong>Email:</strong> ".$email."<br>"; 
-echo "<stong>Message:</strong> ".$message."<br>";
+echo "<strong>Name:</strong>".$name."<br>";
+echo "<strong>Email:</strong>".$email."<br>";
+echo "<strong>Message:</strong>".$message."<br>";
  
 //generate email and send!
 $to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email\n Message \n $message";
-$headers = "From: $myemail\n";
+$email_subject = "Contact form submission from: $name";
+$email_body = "You have received a new message.\n".
+    "Here are the details:\n".
+    "Name: $name\n".
+    "Email: $email\n".
+    "Message:\n $message";
+$headers = "From: $email\n";
 $headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
 }
