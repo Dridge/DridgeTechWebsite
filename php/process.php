5de34e61c6a76ef075ac1b9c463909eee43bedcd
RichardE <richarde@localhost.neversettle.com>
2018-03-19 22:56:50 +0000

    Fix missing message name

--- php/process.php
+++ php/process.php
@@ -1,17 +1,11 @@
 <?php
-//add the recipient's address here
 $myemail = 'richard.eldridge2010@gmail.com';
  
-//grab named inputs from html then post to #thanks
-if (isset($_POST['name'])) {
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
 $name = strip_tags($_POST['name']);
 $email = strip_tags($_POST['email']);
 $message = strip_tags($_POST['message']);
-echo "<span class=\"alert alert-success\" >Your message has been received. Thanks! Here is what you submitted:</span><br><br>";
-echo "<strong>Name:</strong>".$name."<br>";
-echo "<strong>Email:</strong>".$email."<br>";
-echo "<strong>Message:</strong>".$message."<br>";
- 
+
 //generate email and send!
 $to = $myemail;
 $email_subject = "Contact form submission from: $name";
@@ -19,7 +13,7 @@ $email_body = "You have received a new message.\n".
     "Here are the details:\n".
     "Name: $name\n".
     "Email: $email\n".
-    "Message:\n $message";
+    "Message: $message";
 $headers = "From: $email\n";
 $headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
